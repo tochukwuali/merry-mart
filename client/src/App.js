7fadf8228ec375486486805916b6ac77856e5aee
tcee42 <toochukwuali@gmail.com>
2021-01-31 03:12:10 +0100

    Initial commit and half done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { Container } from "react-bootstrap";
+import {Route} from "react-router-dom"
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-     <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' exact component={HomeScreen} />
+          <Route path="/product/:id" exact component={ProductScreen}/>
+          <Route path="/cart/:id?" component={CartScreen}/>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
